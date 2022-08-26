06ef4124ff27eae5c9e7d5aa6812ca449c953677
Ivanilson Ferreira <ivanilson1dj@gmail.com>
2022-08-26 13:38:29 -0300

    1.0.0

---
+++ assets/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    buttonSubmit:{
+        color:"#7D0DFF",
+        fontSize:18,
+        fontWeight:"bold",
+        backgroundColor: "#DB9B32"
+    }
+});
+
+export default styles
+
+/* 
+#7D0DFF
+#60D3DB
+#9BF046
+#DB9B32
+#FF6BAE
+*/
